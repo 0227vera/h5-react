81cd2e0486499a0827e6c8930d3e60fd995990ec
liaoxuan <liaoxuan@sougo-inc.com>
2020-09-18 11:06:44 +0800

    perf: demo

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,22 +2,43 @@ import React from "react"
 import { withRouter } from "react-router-dom"
 import { MobXProviderContext } from "mobx-react"
 import { toJS } from "mobx"
+import styles from "./style/Home.less"
+// import classnames from "classnames/bind"
+
+// const cx = classnames.bind(styles)
 
 const Home = props => {
   const { history } = props
   const { commonStore } = React.useContext(MobXProviderContext)
   const common = toJS(commonStore)
+
+  // 这种使用方式是为了切换class方便，方便做一些动画效果，可以通过state的变量直接控制class的情况
+  // const container = cx({ class1: false, class2: true })
   return (
-    <div>
-      <span>this is home page</span>
-      <span>{JSON.stringify(common)}</span>
-      <button
-        onClick={() => {
-          history.push("/setting")
-        }}
-      >
-        click to go to setting
-      </button>
+    <div className={styles.container}>
+      <div className={styles.contentContainer}>
+        <div className={styles.content}>
+          <h3>this is home page</h3>
+          <span>使用mobx情况:</span>
+          <ul>
+            <li>
+              <span>{`当前人姓名：${common.currentUserInfo.name}`}</span>
+            </li>
+            <li>
+              <span>{`当前人年纪：${common.currentUserInfo.age}`}</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className={styles.footer}>
+        <button
+          onClick={() => {
+            history.push("/setting")
+          }}
+        >
+          click to go back home
+        </button>
+      </div>
     </div>
   )
 }
--- src/pages/Setting/Setting.jsx
+++ src/pages/Setting/Setting.jsx
@@ -1,18 +1,26 @@
 import React from "react"
 import { withRouter } from "react-router-dom"
+import styles from "./style/Setting.less"
 
 const Setting = props => {
   const { history } = props
   return (
-    <div>
-      <span>this is setting page</span>
-      <button
-        onClick={() => {
-          history.goBack()
-        }}
-      >
-        click to go to home
-      </button>
+    <div className={styles.container}>
+      <div className={styles.contentContainer}>
+        <div className={styles.content}>
+          <h3>this is setting page</h3>
+          <span>之后这个地方会加一下已有组件的展示</span>
+        </div>
+      </div>
+      <div className={styles.footer}>
+        <button
+          onClick={() => {
+            history.goBack()
+          }}
+        >
+          click to go to setting
+        </button>
+      </div>
     </div>
   )
 }
