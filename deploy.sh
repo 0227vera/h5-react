5b9a11198678e3ea69316335473e54c6aac92d1b
liaoxuan <liaoxuan@sougo-inc.com>
2020-09-25 11:57:24 +0800

    perf: deploy

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,15 @@
+# 确保脚本抛出遇到的错误
+set -e
+# 建立git，并上传readme到master分支
 git init
-git add -A
+git add README.md
 git commit -m 'init: project'
+git branch -M master
+git remote add origin "{{gitAddress}}"
+git push -u origin master
 
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f "{{gitAddress}}" master
+
+# 建立dev分别
+git branch -M dev
+git checkout dev
+git push -u origin dev
